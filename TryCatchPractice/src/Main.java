58ee86c60c3ebf64cbface09959c8bc405f239f9
sengir92 <onursengir92@gmail.com>
2024-01-07 15:35:57 +0300

    Try Catch Practice Updates

--- TryCatchPractice/src/Main.java
+++ TryCatchPractice/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         // define an array with 10 elements
-        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.print("Enter index number: ");
 
         // input index number from user
@@ -13,14 +13,18 @@ public class Main {
 
         //try/catch method. If user enters bigger number than the length of the array, the reason for the exception will be shown
         try {
-            System.out.println(array[indexNo]);
+            int check = checkIndexElement(array, indexNo);
+            System.out.println(check);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
 
     }
 
-
+    // method that return the entered index number in the array
+    public static int checkIndexElement(int[] arr, int indexNumber) {
+        return arr[indexNumber];
+    }
 
 
 }
