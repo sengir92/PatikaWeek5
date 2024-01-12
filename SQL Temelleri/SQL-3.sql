b9c6c323d6f76d9ed7f93fc19540f1e116bedb65
Onur Sengir <129227744+sengir92@users.noreply.github.com>
2024-01-12 18:51:05 +0300

    Add files via upload

---
+++ SQL Temelleri/SQL-3.sql
@@ -0,0 +1,5 @@
+SELECT country FROM country WHERE country LIKE 'A%a';
+SELECT country FROM country WHERE country LIKE '_____n';
+SELECT title FROM  film WHERE title ILIKE '%t%';
+SELECT * FROM Film WHERE title LIKE 'C%' AND length >90 AND rental_rate = 2.99;
+
